434028197c235649f30546b2992adeed21641961
valeriasusenaceva <valerapopova55@gmail.com>
2023-06-02 22:53:24 +0500

    Добавили метод allMoneyByAllClients

--- src/com/java/banksySytem/Operations.java
+++ src/com/java/banksySytem/Operations.java
@@ -25,4 +25,66 @@ public class Operations {
         }
 
     }
+
+    public static void transaction3ByArray(String nameByPersone1, String nameByPersone2,
+                                           int ammount, Persone[] persones) {
+        Persone extraPersone1 = null;
+        Persone extraPersone2 = null;
+        for (int i = 0; i < persones.length; i++) {
+            if (nameByPersone1.equals(persones[i].getName())) {
+                extraPersone1 = persones[i];
+            }
+        }
+        for (int i = 0; i < persones.length; i++) {
+            if (nameByPersone2.equals(persones[i].getName())) {
+                extraPersone2 = persones[i];
+            }
+        }
+        int extraBillId1 = 0;
+        Bill extraBill1 = null;// просто счет, факт существования счета у человека
+        for (int i = 0; i < extraPersone1.bills.size(); i++) {
+            if ((extraPersone1.bills.get(i).getBillId() > extraBillId1) && (extraPersone1.bills.get(i).getAmmount() > ammount)) {
+                extraBillId1 = extraPersone1.bills.get(i).getBillId();
+                extraBill1 = extraPersone1.bills.get(i);
+            }
+        }
+
+        int extraBillId2 = 0;
+        Bill extraBill2 = null;
+        for (int i = 0; i < extraPersone2.bills.size(); i++) {
+            if (extraPersone2.bills.get(i).getBillId() > extraBillId2) {
+                extraBillId2 = extraPersone2.bills.get(i).getBillId();
+                extraBill2 = extraPersone2.bills.get(i);
+            }
+
+        }
+        if (extraBill1.getAmmount() < ammount) {
+            System.out.println("На вашем счете недостаточно средств");
+        } else {
+            int sum2 = extraBill2.getAmmount() + ammount;
+            int sum1 = extraBill1.getAmmount() - ammount;
+            extraBill1.setAmmount(sum1);
+            extraBill2.setAmmount(sum2);
+        }
+
+    }
+
+    public static void transaction4(Persone persone1, Persone persone2, int ammount) {
+        int finalSumPersone1 = 0;
+        int finalSumPersone2 = 0;
+        Bill extraBill1 = persone1.mainBill;
+        Bill extraBill2 = persone2.mainBill;
+
+
+        finalSumPersone1 = extraBill1.getAmmount() - ammount;
+        finalSumPersone2 = extraBill2.getAmmount() + ammount;
+        if (extraBill1.getAmmount() < ammount) {
+            System.out.println("На вашем счете недостаточно средств");
+        } else {
+            extraBill1.setAmmount(finalSumPersone1);
+            extraBill2.setAmmount(finalSumPersone2);
+
+
+        }
+    }
 }
